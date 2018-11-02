c998772ce336ceaa0d653ea0f462322e947c53fc
Brad Niedt <bradleyniedt@gmail.com>
2018-11-01 18:42:39 -0600

    adds test for year attr reader

--- test/vehicle_test.rb
+++ test/vehicle_test.rb
@@ -5,7 +5,12 @@ require './lib/vehicle'
 
 class VehicleTest < Minitest::Test
   def test_it_exists
-    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    vehicle = Vehicle.new('2001', 'Honda', 'Civic')
     assert_instance_of Vehicle, vehicle
+  end
+
+  def test_it_has_a_year
+    vehicle = Vehicle.new('2001', 'Honda', 'Civic')
+    assert_equal '2001', vehicle.year 
   end
 end
